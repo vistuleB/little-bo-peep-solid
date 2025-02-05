c63cb70f0454497a48e5829e0513febe91ddd8d5
JOHN STEINBERGER <jpsteinb@gmail.com>
2025-02-05 18:26:24 +0100

    more debug

--- src/components/TOCAuthorSuppliedContent.tsx
+++ src/components/TOCAuthorSuppliedContent.tsx
@@ -10,40 +10,9 @@ const TOCAuthorSuppliedContent = () => {
         <ul>
           <TOCItem
             article_type={1}
-            label="A Few Refreshers"
-            on_mobile="A Few Refreshers"
-            href="chapter1" />
-          <TOCItem
-            article_type={2}
-            label="Slopes"
-            on_mobile="Slopes"
-            href="chapter2" />
-          <TOCItem
-            article_type={3}
-            label="Functions"
-            on_mobile="Functions"
-            href="chapter3" />
-          <TOCItem
-            article_type={4}
             label="Derivatives"
             on_mobile="Derivatives"
-            href="chapter4" />
-        </ul>
-      </div>
-      <Spacer />
-      <div id="bootcamp">
-        <TOCTitle label="Bootcamps" />
-        <ul>
-          <TOCItem
-            article_type={1}
-            label="Sets"
-            on_mobile="Sets"
-            href="bootcamp1" />
-          <TOCItem
-            article_type={2}
-            label="Powers of 10"
-            on_mobile="Powers of 10"
-            href="bootcamp2" />
+            href="chapter1" />
         </ul>
       </div>
     </>
