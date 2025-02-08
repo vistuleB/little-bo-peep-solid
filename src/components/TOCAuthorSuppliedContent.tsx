603636f55b795e148009db1d1f72787a02418121
JOHN STEINBERGER <jpsteinb@gmail.com>
2025-02-09 00:34:09 +0100

    forgot to add content...

--- src/components/TOCAuthorSuppliedContent.tsx
+++ src/components/TOCAuthorSuppliedContent.tsx
@@ -10,9 +10,40 @@ const TOCAuthorSuppliedContent = () => {
         <ul>
           <TOCItem
             article_type={1}
+            label="A Few Refreshers"
+            on_mobile="A Few Refreshers"
+            href="chapter1" />
+          <TOCItem
+            article_type={2}
             label="Slopes"
             on_mobile="Slopes"
-            href="chapter1" />
+            href="chapter2" />
+          <TOCItem
+            article_type={3}
+            label="Functions"
+            on_mobile="Functions"
+            href="chapter3" />
+          <TOCItem
+            article_type={4}
+            label="Derivatives"
+            on_mobile="Derivatives"
+            href="chapter4" />
+        </ul>
+      </div>
+      <Spacer />
+      <div id="bootcamp">
+        <TOCTitle label="Bootcamps" />
+        <ul>
+          <TOCItem
+            article_type={1}
+            label="Sets"
+            on_mobile="Sets"
+            href="bootcamp1" />
+          <TOCItem
+            article_type={2}
+            label="Powers of 10"
+            on_mobile="Powers of 10"
+            href="bootcamp2" />
         </ul>
       </div>
     </>
