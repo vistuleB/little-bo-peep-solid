951d8c2fc842cc3838ac1265c026286335e496a2
JOHN STEINBERGER <jpsteinb@gmail.com>
2025-02-19 11:09:07 +0100

    add hack to divide 'The Trigonometric Functions' title on 2 lines

--- src/components/TOCItem.tsx
+++ src/components/TOCItem.tsx
@@ -1,5 +1,23 @@
+import { createEffect, createSignal, JSXElement, onMount } from "solid-js";
+import { DOMElement } from "solid-js/jsx-runtime";
 import { MOBILE_MAX_WIDTH, DESKTOP_COLUMN_WIDTH } from "~/constants";
 import { useGlobalContext } from "~/store/StoreProvider";
+
+const setUpParentAndWidth = (
+  parent: HTMLDivElement,
+  span: HTMLSpanElement
+):void => {
+  parent.style.width = MOBILE_MAX_WIDTH + "px";
+  parent.style.position = 'absolute';
+  parent.style.visibility = 'hidden';
+  span.style.font = "Baskerville, serif";
+  span.style.fontSize = "1.875rem";
+  span.style.height = 'auto';
+  span.style.width = 'auto';
+  span.style.whiteSpace = 'nowrap';
+  parent.appendChild(span);
+  document.body.appendChild(parent);
+}
 
 const TOCItem = (props: {
   href: string;
@@ -8,20 +26,62 @@ const TOCItem = (props: {
   article_type: any;
 }) => {
   let { store, } = useGlobalContext();
+  const [our_width, set_our_width] = createSignal(store.innerWidth > MOBILE_MAX_WIDTH ? DESKTOP_COLUMN_WIDTH : store.innerWidth);
+
+  const parentDiv = document.createElement("div");
+  const textSpan = document.createElement("span");
+  setUpParentAndWidth(parentDiv, textSpan);
+
+  const getTextWidth = (text: string):number => {
+    textSpan.innerHTML = text;
+    return Math.ceil(textSpan.offsetWidth);
+  }
+
+  const [first_half, set_first_half] = createSignal(props.label);
+  const [second_half, set_second_half] = createSignal("");
+
+  // for now just a hack, too lazy to set up full
+  // solution:
+  createEffect(() => {
+    set_our_width(store.innerWidth > MOBILE_MAX_WIDTH ? DESKTOP_COLUMN_WIDTH : store.innerWidth);
+    if (props.label === "The Trigonometric Functions") {
+      if (getTextWidth(props.label) > our_width() - 60) {
+        set_first_half("The Trigonometric");
+        set_second_half("Functions");
+      } else {
+        set_first_half("The Trigonometric Functions");
+        set_second_half("");
+      }
+    }
+  });
 
   return (
-    <a
-      href={`/article/${props.href}`}
-      class="text-column flex items-baseline justify-between !leading-9 lg:!leading-10 !text-3xl"
-      style={`width:${store.innerWidth > MOBILE_MAX_WIDTH ? DESKTOP_COLUMN_WIDTH : store.innerWidth}px;`}
-    >
-      <span class="block !w-fit">{props.article_type}</span>
-      <span class="dots !w-auto"></span>
-      <span class="sm:hidden !w-auto">
-        {props.on_mobile == "" ? props.label : props.on_mobile}
-      </span>
-      <span class="hidden sm:block !w-auto">{props.label}</span>
-    </a>
+    <>
+      <a
+        href={`/article/${props.href}`}
+        class="text-column flex items-baseline justify-between !leading-9 lg:!leading-10 text-3xl"
+        style={`width:${our_width()}px;`}
+      >
+        <span class="block !w-fit">{props.article_type}</span>
+        <span class="dots !w-auto"></span>
+        <span class="text-right">{first_half()}</span>
+      </a>
+      {
+        (second_half() === "") ? (
+          <></>
+        ) : (
+          <a
+            href={`/article/${props.href}`}
+            class="text-column flex items-baseline justify-between !leading-9 lg:!leading-10 text-3xl"
+            style={`width:${our_width()}px;`}
+          >
+            <span class="dots !w-auto"></span>
+            {/* <span class="!w-auto"></span> */}
+            <span class="text-right">{second_half()}</span>
+          </a>
+        )
+      }
+    </>
   );
 };
 
