bacf728d8c448289f75307ef4937124a013fd210
MrChaker <mr.chakerw@gmail.com>
2025-03-12 16:42:34 +0100

    Change env var

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -7,8 +7,8 @@ import PanelTitle from "./PanelTitle";
 const Panel = () => {
   const { store } = useGlobalContext();
   const menu_closed = () => !store.panel_opened;
-  const env = import.meta.env.VITE_ENV;
-  console.log(env)
+  const dev = import.meta.env.DEV;
+  console.log(dev)
 
   const toggle_scroll = (overflow: string) => {
     if (menu_closed()) {
@@ -44,7 +44,7 @@ const Panel = () => {
         <div class="select-none scrollbar-hidden sm:h-full pt-[0.6em] px-[1em] overflow-y-hidden [&ul]:mb-[8px] [&ul]:p-0">
           <PanelTableOfContents />
           {
-          env === "development" &&  
+          dev &&  
             <div id="options">
               <PanelTitle label="Options" />
               <Option label="Areas" state_key="show_areas" />
