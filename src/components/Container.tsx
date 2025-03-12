89a0a6ccc34f86647235af826c4ee428e12125e9
MrChaker <mr.chakerw@gmail.com>
2025-03-12 16:31:39 +0100

    Handle no exercises case

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -126,6 +126,7 @@ const Container = (props: ParentProps) => {
 
     // const savedScrollY = window.scrollY
     const exerciseBtnsPos = () => {
+      if (!document.getElementById("exercises-btns")) return document.body.scrollHeight
       const rect = document.getElementById("exercises-btns")?.getBoundingClientRect()!;
       return  (rect.y) + window.scrollY + (rect.height * 2)
     }
