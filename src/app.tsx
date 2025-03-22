51874558bdca6bde51e216ca16ab9c6072fa4b51
JOHN STEINBERGER <jpsteinb@gmail.com>
2025-03-22 15:15:00 +0100

    trying to add Meta og:image image for link preview

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { ParentProps, Suspense } from "solid-js";
 import Container from "~/components/Container";
+import { Meta, MetaProvider } from "@solidjs/meta";
 
 import "./app.css";
 import { StoreProvider } from "./store/StoreProvider";
@@ -9,21 +10,23 @@ import PanelButton from "./components/PanelButton";
 import Panel from "./components/Panel";
 
 export default function App() {
-
   return (
-    <StoreProvider>
-      <Router 
-        root={(props) => (
-          <>
-            <PanelButton />
-            <Panel />
-            <Container>
-              <Suspense>{props.children}</Suspense>
-            </Container>
-          </>
-        )}>
-        <FileRoutes />
-      </Router>
-    </StoreProvider>
+    <MetaProvider>
+      <Meta property="og:image" content="images/lbp_preview.png"></Meta>
+      <StoreProvider>
+        <Router 
+          root={(props) => (
+            <>
+              <PanelButton />
+              <Panel />
+              <Container>
+                <Suspense>{props.children}</Suspense>
+              </Container>
+            </>
+          )}>
+          <FileRoutes />
+        </Router>
+      </StoreProvider>
+    </MetaProvider>
   );
 }
