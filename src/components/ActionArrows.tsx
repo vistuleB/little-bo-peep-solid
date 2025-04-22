1ddfc2c474194ea126ca51b5602dd720130a7c08
JOHN STEINBERGER <jpsteinb@gmail.com>
2025-04-22 15:26:18 +0530

    forgot to run JSX prettifier

--- src/components/ActionArrows.tsx
+++ src/components/ActionArrows.tsx
@@ -65,7 +65,8 @@ const ActionArrows = () => {
         opacity: hovered() ? 1 : opacity(),
         left: `${1500 - store.scrollX - 33}px`,
       }}
-      class="fixed bottom-3">
+      class="fixed bottom-3"
+    >
       <button
         onClick={handleUpClick}
         style={{
@@ -76,8 +77,9 @@ const ActionArrows = () => {
           store.scrollY > 1
             ? "stroke-black hover:stroke-stone-600"
             : "stroke-stone-300",
-          "transition-all",
-        )}>
+          "transition-all"
+        )}
+      >
         <DoubleUpArrowSVG />
       </button>
       <button
@@ -90,8 +92,9 @@ const ActionArrows = () => {
           store.scrollY + store.innerHeight - store.scrollHeight < -1
             ? "stroke-black hover:stroke-stone-600"
             : "stroke-stone-300",
-          "transition-all",
-        )}>
+          "transition-all"
+        )}
+      >
         <DoubleDownArrowSVG />
       </button>
     </div>
@@ -109,9 +112,15 @@ const DoubleUpArrowSVG = (props: { class?: string; style?: string }) => {
   const { store } = useGlobalContext();
 
   return (
-    <svg style={{
-      "background-color": store.show_areas ? "#ffc0cb" : "transparent",
-    }} class={props.class} width="30" height="30" viewBox="0 5 30 35">
+    <svg
+      style={{
+        "background-color": store.show_areas ? "#ffc0cb" : "transparent",
+      }}
+      class={props.class}
+      width="30"
+      height="30"
+      viewBox="0 5 30 35"
+    >
       <path
         d={`
           M${un - adx} ${un + cdy + ady} l${adx} ${-ady} ${adx} ${ady}
@@ -129,9 +138,15 @@ const DoubleDownArrowSVG = (props: { class?: string; style?: string }) => {
   const { store } = useGlobalContext();
 
   return (
-    <svg style={{
-      "background-color": store.show_areas ? "#ffc0cb" : "transparent",
-    }} class={props.class} width="30" height="30" viewBox="0 -15 30 35">
+    <svg
+      style={{
+        "background-color": store.show_areas ? "#ffc0cb" : "transparent",
+      }}
+      class={props.class}
+      width="30"
+      height="30"
+      viewBox="0 -15 30 35"
+    >
       <path
         d={`
           M${un - adx} ${un - cdy - ady} l${adx} ${ady} ${adx} ${-ady}
@@ -139,7 +154,8 @@ const DoubleDownArrowSVG = (props: { class?: string; style?: string }) => {
         `}
         stroke-linecap="round"
         stroke-width={sw}
-        fill="none"></path>
+        fill="none"
+      ></path>
     </svg>
   );
 };
