f0b721fd5575f5098370ce96556b634606edcf01
MrChaker <mr.chakerw@gmail.com>
2025-05-19 13:14:13 +0100

    Add supprt for different icons

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -9,10 +9,51 @@ export default createHandler(() => (
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           {import.meta.env.VITE_ENV === "DEV" ? (
-            <link rel="icon" href="/favicon_dev.svg" />
+            <>
+              <link rel="icon" href="/favicon-dev.ico" />
+              <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href="/apple-touch-icon-dev.png"
+              />
+              <link
+                rel="apple-touch-startup-image"
+                href="/apple-touch-icon-dev.png"
+              />
+              <link
+                rel="mask-icon"
+                href="/safari-pinned-tab-dev.svg"
+                color="#5bbad5"
+              />
+              <link
+                rel="apple-touch-icon-precomposed"
+                href="/apple-touch-icon-dev.png"
+              />
+            </>
           ) : (
-            <link rel="icon" href="/favicon_prod.svg" />
+            <>
+              <link rel="icon" href="/favicon.ico" />
+              <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href="/apple-touch-icon.png"
+              />
+              <link
+                rel="apple-touch-startup-image"
+                href="/apple-touch-icon.png"
+              />
+              <link
+                rel="mask-icon"
+                href="/safari-pinned-tab.svg"
+                color="#5bbad5"
+              />
+              <link
+                rel="apple-touch-icon-precomposed"
+                href="/apple-touch-icon.png"
+              />
+            </>
           )}
+
           <meta
             name="viewport"
             content="width=device-width,initial-scale=1.0,minimum-scale=1"
