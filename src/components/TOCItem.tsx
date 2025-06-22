1bd629ee5ff1223f5789ce4b9a756c4cc197dd6b
JOHN STEINBERGER <jpsteinb@gmail.com>
2025-06-22 12:59:09 +0800

    squash me

--- src/components/TOCItem.tsx
+++ src/components/TOCItem.tsx
@@ -19,14 +19,14 @@ const TOCItem = (props: {
       class="relative m-auto leading-[2.4rem] text-3xl"
       style={`width:${our_width() - 32}px;direction:rtl;`}>
         <div class="toc-item-lead-wrapper">
-          <div> {/* somehow this wrapper div is useful for base-alignment */}
+          <div>
             <span>{props.article_type}</span>
             <span class="toc-item-lead-dots">..........................................................................................................................................................................</span>
           </div>
         </div>
         <div class="toc-item-title-outline" style="--toc-label-stroke-color: white" aria-hidden="true">{props.label}</div>
         <div class="toc-item-title">
-        <a href={`/article/${props.href}`}><span>{props.label}</span></a>
+          <a href={`/article/${props.href}`}><span>{props.label}</span></a>
         </div>
     </div>
   );
