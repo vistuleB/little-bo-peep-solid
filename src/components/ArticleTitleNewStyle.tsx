4b3c8ac29c0c2115db99070ee57579f8a73bfab1
JOHN STEINBERGER <jpsteinb@gmail.com>
2025-06-25 19:30:20 +0800

    change 'slice' to 'text-column'

--- src/components/ArticleTitleNewStyle.tsx
+++ src/components/ArticleTitleNewStyle.tsx
@@ -14,7 +14,7 @@ const ArticleTitleNewStyle = (props: ParentProps & {
   const { on_mobile } = useOnMobile();
 
   return (
-    <div class="slice" style={`width:${mainColumnWidth()}px;`}>
+    <div class="text-column" style={`width:${mainColumnWidth()}px;`}>
       <h1
         class="text-2rem sm:text-4xl pt-5 sm:pt-4 pb-4"
         style={`margin-top: ${
