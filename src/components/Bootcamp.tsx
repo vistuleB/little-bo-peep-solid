8429ca628e2dbc36c94a4adf0793dd805b465140
JOHN STEINBERGER <jpsteinb@gmail.com>
2025-06-27 22:47:55 +0800

    fix props in Bootcamp component

--- src/components/Bootcamp.tsx
+++ src/components/Bootcamp.tsx
@@ -4,10 +4,10 @@ const Bootcamp = (props: any) => {
   return (
     <AbstractArticle
       id={props.id}
-      pageNecessaryMargin={props.pageNecessaryMargin}
-      maxElementWidth={props.maxElementWidth}
-      prevPage={props.prevPage}
-      nextPage={props.nextPage}
+      pageNecessaryMargin={props["page-necessary-margin"]}
+      maxElementWidth={props["max-element-width"]}
+      prevPage={props["prev-page"]}
+      nextPage={props["next-page"]}
     >
       {props.children}
     </AbstractArticle>
