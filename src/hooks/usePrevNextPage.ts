aafe01b090b5532c77ba4eaf6d24899139b25955
JOHN STEINBERGER <jpsteinb@gmail.com>
2026-01-05 10:10:24 +0100

    long comment usePrevNextPage.tsx

--- src/hooks/usePrevNextPage.ts
+++ src/hooks/usePrevNextPage.ts
@@ -11,14 +11,13 @@ const usePrevNextPage = () => {
   const getPrevPage = () => getPage(store.prevPage);
   const getNextPage = () => getPage(store.nextPage);
   const getPage = (page: string) => {
-    if (page === "") return;
-
-    // if navigating to the same route, force a reload
-    if (location.pathname === page) {
-      // clear loading immediately since we're not actually navigating
-      set_store("loading", false);
-      return;
-    }
+    if (page === "" || location.pathname === page) return;
+    // note: if a value of 'page' is given such that location.pathname != page
+    // but such that the router still resolves 'page' to the current page
+    // we will get an infinite rabbit, because 'onMount' is never called and
+    // the rabbit is never cleared; so you always need to call getPage with carefully
+    // normalized, 'correct' paths!!! (or with paths that certifiably point to a
+    // different page)
     clearCurrentPage();
     if (store.navigation_delays) {
       setTimeout(
