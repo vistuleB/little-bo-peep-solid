6787cca985a6cd549cd00b58be1ac6986d238723
Azizul Karim <karimazizul1@gmail.com>
2026-03-05 20:02:16 +0600

    wly-98: fix 404 image

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -84,7 +84,7 @@ export default function NotFound() {
   return (
     <div class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <img
-        src="/non-build-img/404_gh.png"
+        src="/non-build-img/404.png"
         alt="404 - Page not found"
         style={`max-width:${mainColumnWidth() * 0.7}px;max-height:65vh;`}
       />
