71d1c9a915456c1450fc00bb380b3c493ff28d12
Azizul Karim <karimazizul1@gmail.com>
2026-05-16 17:58:02 +0600

    wly-167: bug-fix: headerBlob image gets offsetHeight on initial load

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,4 @@
-import { createEffect, createSignal } from "solid-js";
+import { createEffect, createSignal, onMount } from "solid-js";
 import { useGlobalContext } from "~/store/StoreProvider";
 import {
   DESKTOP_COLUMN_WIDTH,
@@ -25,7 +25,8 @@ const Nav = () => {
         )}
         onClick={(e) => {
           e.stopPropagation();
-        }}>
+        }}
+      >
         <div class="relative select-none border-[var(--nav-border)] border-b bg-bg z-40 w-full h-14 left-0">
           <Title />
         </div>
@@ -39,10 +40,22 @@ const Title = () => {
   const [_, setRoute] = createSignal("/");
   const { getPage } = usePrevNextPage();
   const { store } = useGlobalContext();
-  let headerBlob: HTMLAnchorElement | undefined = undefined;
+  let headerBlob!: HTMLAnchorElement;
+  const [imageLoaded, setImageLoaded] = createSignal(false);
+
+  onMount(() => {
+    // we need to make sure headerBlob svg image gets proper offsetHeight on initial load
+    const img = headerBlob?.querySelector("img");
+    if (!img) return;
+    if (img.complete) {
+      setImageLoaded(true);
+    } else {
+      img.addEventListener("load", () => setImageLoaded(true), { once: true });
+    }
+  });
 
   createEffect(() => {
-    if (!headerBlob) return;
+    if (!headerBlob || !imageLoaded()) return;
 
     const blob = headerBlob as HTMLAnchorElement;
     const contWidth = containerWidth();
@@ -67,7 +80,8 @@ const Title = () => {
       class="block mr-auto absolute origin-top-left"
       ref={headerBlob}
       href="/"
-      onClick={() => getPage("/")}>
+      onClick={() => getPage("/")}
+    >
       <HeaderBlob />
     </a>
   );
