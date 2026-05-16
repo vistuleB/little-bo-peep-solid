b66256719f90f6480a654fab2f33e2237740356d
Azizul Karim <karimazizul1@gmail.com>
2026-05-16 19:40:28 +0600

    wly-167: remove maxElementWidth

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -73,7 +73,6 @@ export default function NotFound() {
 
   onMount(() => {
     set_store("pageNecessaryMargin", 0);
-    set_store("maxElementWidth", 0);
 
     window.addEventListener("keydown", handleKeydown);
     window.addEventListener("resize", handleResize);
