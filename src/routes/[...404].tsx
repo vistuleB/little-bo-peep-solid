8df45d125b3e52d0f8ed6199b10a36ef08af697e
Azizul Karim <karimazizul1@gmail.com>
2026-05-18 18:04:12 +0600

    wly-167: fix scrollbar issue

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -54,6 +54,7 @@ export default function NotFound() {
 
   onMount(() => {
     set_store("pageNecessaryMargin", 0);
+    set_store("maxElementWidth", 0);
 
     window.addEventListener("keydown", handleKeydown);
     window.addEventListener("resize", handleResize);
