481e040afbf3a8ef5cd45281813a333f80b63f76
JOHN STEINBERGER <jpsteinb@gmail.com>
2026-07-16 17:08:44 +0200

    Remove page panning test scaffolding

--- src/routes/article/bootcamp1.tsx
+++ src/routes/article/bootcamp1.tsx
@@ -5,6 +5,7 @@ import InChapterLink  from "~/components/InChapterLink";
 import { MathBlock, Math }  from "~/components/Math";
 import OuterP  from "~/components/OuterP";
 import SectionsBreadcrumbs,  { BreadcrumbItem }  from "~/components/SectionsBreadcrumbs";
+import { ImageRight }  from "~/components/SideImage";
 import { Pause, NoBreak, Section }  from "~/components/Wrappers";
 import useShowMore from "~/hooks/useShowMore";
 
@@ -599,6 +600,13 @@ const Rest = () => {
           $$
           \Large \&#123;\&#123;\&#125; \&#125; \ne \&#123;\&#125;
           $$
+          <ImageRight
+            src="/build-img/svgo-svg/dcQB.svg"
+            offsetX="3em"
+            atLeastAsWide={true}
+            intrinsicWidth={300}
+            intrinsicHeight={150}
+          />
         </MathBlock>
         <Pause />
         <OuterP>
